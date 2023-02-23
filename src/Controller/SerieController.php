30a1a20304da15db478aa51522575d8e40109402
mmongis2022 <mongis.maximepp@gmail.com>
2023-02-23 09:49:21 +0100

    commit des images

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,22 +12,38 @@ use Symfony\Component\Routing\Annotation\Route;
 class SerieController extends AbstractController
 {
     #[Route('/list', name: 'list_')]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
         //TODO Récupéerer la liste des series en BDD
-        return $this->render('serie/list.html.twig',);
+        //on récupère toutes les series en passant le repository
+        //$series = $serieRepository->findAll();
+
+        //utilisation de findBY avec un tableau de classe where, order by
+        //$series = $serieRepository->findBy(["status" =>"ended"], ["popularity" => 'DESC']);
+
+        $series = $serieRepository->findBy([], ["vote" => 'DESC'], limit: 50);
+        dump($series);
+        return $this->render('serie/list.html.twig', [
+            //on envoie les données à la vue
+            'series' => $series
+        ]);
     }
-    #[Route('/{id}', name: 'show', requirements: ['id'=>'\d+'])]
-    public function show(int $id): Response
+
+    #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
+    public function show(int $id, SerieRepository $serieRepository): Response
     {
-        dump($id);
+        $serie = $serieRepository->find($id);
+        dump($serie);
         //TODO Créer un formulaire d'ajout de série
-        return $this->render('serie/show.html.twig');
+        return $this->render('serie/show.html.twig', [
+            'serie' => $serie
+        ]);
     }
+
     #[Route('/add', name: 'add')]
-    public function add(): Response
+    public function add(SerieRepository $serieRepository, EntityManagerInterface $entityManager): Response
     {
-        dump(1231456);
+
         //TODO Créer un formulaire d'ajout de série
         return $this->render('serie/add.html.twig');
     }
